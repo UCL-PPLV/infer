feed36fa6668df6d1ee66075dc85880e2adc50cd
ben <ben.loh.15@ucl.ac.uk>
2017-07-20 18:04:01 +0100

    Synthesis using given preposts should work now, not heavily tested yet - also, although pointers being copied in the post is checked, there is no synthesis procedure for it (e.g. pre: x |-> a * y |-> b and post: x |-> y * y |-> b)

--- infer/src/synthesis/synthesize.ml
+++ infer/src/synthesis/synthesize.ml
@@ -55,6 +55,26 @@ let print_specs specs =
     ) posts;
   ) specs
 
+let print_node_instrs proc_desc = 
+  let rec print_nodes node = 
+    Procdesc.Node.pp_instrs Pp.text ~sub_instrs:true None F.std_formatter node;
+    F.print_string "preds: ";
+    List.iter ~f:(fun p -> 
+      Procdesc.Node.pp F.std_formatter p; F.print_string " ") (Procdesc.Node.get_preds node);
+    F.print_string "; node: ";
+    Procdesc.Node.pp F.std_formatter node;
+    F.print_string "; succs: ";
+    List.iter ~f:(fun s -> 
+      Procdesc.Node.pp F.std_formatter s; F.print_string " ") (Procdesc.Node.get_succs node);
+    F.print_string "\n\n";
+    match Procdesc.Node.get_kind node with
+    | Procdesc.Node.Exit_node _ -> () 
+    | _ -> print_nodes (List.hd_exn (Procdesc.Node.get_succs node))
+  in
+  let start_node = Procdesc.get_start_node proc_desc in 
+  print_nodes start_node;
+  F.print_string "\n"
+
 (* Create a alias list of Exp (temp var) * Pvar (real var) from a sigma (of a pre) *)
 let create_pvar_env_list (sigma: Prop.sigma) : (Exp.t * Pvar.t) list =
   let env = ref [] in
@@ -70,7 +90,7 @@ let create_pvar_env_list (sigma: Prop.sigma) : (Exp.t * Pvar.t) list =
 let find_exp_replacement (name: string) (exp_replace_list: (Exp.t * Pvar.t) list) = 
   match List.find ~f:(fun p -> String.equal (Pvar.get_simplified_name (snd p)) name) 
       exp_replace_list with
-  | Some pair -> fst pair
+  | Some pair -> pair
   | None -> failwith "find_exp_replacement: No var of that name found"
 
 (* Find the Exp that an Exp points to in a sigma *)
@@ -78,21 +98,6 @@ let find_pointsto (e: Exp.t) sigma =
   match List.find ~f:(fun h -> Exp.equal (Sil.hpred_get_lhs h) (e)) sigma with 
   | Some Sil.Hpointsto (_, Eexp (Var v, _), _) -> Exp.Var v
   | _ -> failwith "find_pointsto: No value found"
-
-let create_new_post tenv (pre: Prop.normal Prop.t) = 
-  let exp_replace_list = create_pvar_env_list pre.sigma in
-  let x_val = find_exp_replacement "x" exp_replace_list in
-  let y_val = find_exp_replacement "y" exp_replace_list in
-  let x_pointsto = find_pointsto x_val pre.sigma in
-  let y_pointsto = find_pointsto y_val pre.sigma in 
-  let my_new_hpred1 = Prop.mk_ptsto tenv x_val (Sil.Eexp (y_pointsto, Sil.inst_none)) 
-    (Exp.Sizeof 
-      {typ=(Typ.mk (Tint IInt)); nbytes=None; dynamic_length=None; subtype=Subtype.exact}) in
-  let my_new_hpred2 = Prop.mk_ptsto tenv y_val (Sil.Eexp (x_pointsto, Sil.inst_none)) 
-    (Exp.Sizeof
-      {typ=(Typ.mk (Tint IInt)); nbytes=None; dynamic_length=None; subtype=Subtype.exact}) in
-  let my_new_sigma = my_new_hpred1 :: my_new_hpred2 :: [] in
-  Prop.from_sigma my_new_sigma
 
 let insert_penultimate_node node proc_desc = 
   Procdesc.node_set_succs_exn proc_desc 
@@ -108,26 +113,95 @@ let remove_nullifys proc_desc =
     Procdesc.Node.replace_instrs n new_instrs;
   ) proc_desc
 
-let print_node_instrs proc_desc = 
-  List.iter ~f:(fun n ->
-    Procdesc.Node.pp_instrs Pp.text ~sub_instrs:true None F.std_formatter n;
-    F.print_string "preds: ";
-    List.iter ~f:(fun p -> 
-      Procdesc.Node.pp F.std_formatter p; F.print_string " ") (Procdesc.Node.get_preds n);
-    F.print_string "; node: ";
-    Procdesc.Node.pp F.std_formatter n;
-    F.print_string "; succs: ";
-    List.iter ~f:(fun s -> 
-      Procdesc.Node.pp F.std_formatter s; F.print_string " ") (Procdesc.Node.get_succs n);
-    F.print_string "\n"
-  ) (Procdesc.get_nodes proc_desc);
-  F.print_string "\n"
-
 let get_typ_from_ptr_exn (ptr_typ: Typ.t) = match ptr_typ.desc with
   | Tptr (t, _) -> t 
   | _ -> failwith "Not a pointer type"
 
-let rec synthesize_writes callbacks node (queue: Sil.instr list list) =
+let make_post (procspec: Parsetree.procspec) (actual_pre: Prop.normal Prop.t) tenv proc_desc = 
+  let raw_pre = procspec.pre in 
+  let raw_post = procspec.post in
+  let filter_emp_hpreds = List.filter ~f:(function
+    | Parsetree.Hpred_empty -> false
+    | _ -> true) in
+  let non_empty_post_sigma = filter_emp_hpreds raw_post.sigma in 
+  match non_empty_post_sigma with 
+  | [] -> Prop.expose Prop.prop_emp
+  | _ -> 
+  let non_empty_pre_sigma = filter_emp_hpreds raw_pre.sigma in
+  let quantified_vars = List.map ~f:(function
+    | Parsetree.Hpred_hpointsto (s, _) -> s
+    | _ -> failwith "Case not handled - not pointsto"
+  ) non_empty_pre_sigma in
+
+  (* Get alias, pointsto, and typ of each quantified variable *)
+  let exp_replace_list = create_pvar_env_list actual_pre.sigma in
+  let pvars = List.map ~f:(fun (mang, typ) -> 
+    let pvar = (snd (List.find_exn ~f:(fun p -> 
+      Mangled.equal (Pvar.get_name (snd p)) mang) exp_replace_list)) in
+    (pvar, typ)
+  ) (Procdesc.get_formals proc_desc) in 
+  let param_pointsto_list = List.filter_map ~f:(ident) (List.map ~f:(fun quant -> 
+    let found_alias = List.find ~f:(fun (_, p) -> 
+      String.equal (Pvar.get_simplified_name p) quant) exp_replace_list in
+    match found_alias with 
+    | None -> None (* Prop in spec is not in proc params - ignore *)
+    | Some (param, pvar) -> 
+      let pointsto = find_pointsto param actual_pre.sigma in 
+      let typ = snd (List.find_exn ~f:(fun p -> Pvar.equal (fst p) pvar) pvars) in 
+      Some (quant, param, pointsto, typ)
+  ) quantified_vars) in 
+
+  let hpred_list = List.filter_map ~f:(ident) (List.map ~f:(function 
+    | Parsetree.Hpred_hpointsto (p, q) -> begin
+      let found_in_pre = List.find ~f:(function 
+        | Parsetree.Hpred_hpointsto (_, n) -> String.equal n q
+        | _ -> failwith "Case not handled - not pointsto"
+      ) non_empty_pre_sigma in
+      match found_in_pre with 
+      | Some Parsetree.Hpred_hpointsto (m, n) -> begin
+        let found_param = List.find ~f:(fun (name, _, _, _) ->
+          String.equal p name) param_pointsto_list in
+        match found_param with 
+        | None -> 
+          if not (String.equal p m) then 
+            failwithf "Var %s modified in preposts but not passed in function params" p
+          else None
+        | Some (_, param, _, _) -> 
+          let found_pointsto = List.find ~f:(fun (name, _, _, _) -> 
+            String.equal m name) param_pointsto_list in
+          match found_pointsto with 
+          | None -> 
+            if not (String.equal p m) then 
+              failwithf "Var %s pointsto given in post but is not in pre" p
+            else None
+          | Some (_, _, pointsto, typ) -> 
+            Some (Prop.mk_ptsto tenv param (Sil.Eexp (pointsto, Sil.inst_none))
+            (Exp.Sizeof {typ=(get_typ_from_ptr_exn typ); nbytes=None; 
+              dynamic_length=None; subtype=Subtype.exact}))
+        end
+
+      | None -> begin
+        let found_in_params = List.find ~f:(fun (quant2, _, _, _) 
+          -> String.equal q quant2) param_pointsto_list in 
+        match found_in_params with
+        | None -> None (* Pointsto is in post but not in pre *)
+        | Some (_, param2, _, typ) ->
+          let _, param1, _, _ = List.find_exn ~f:(fun (quant1, _, _, _) -> 
+            String.equal quant1 p) param_pointsto_list in
+          Some (Prop.mk_ptsto tenv param1 (Sil.Eexp (param2, Sil.inst_none))
+            (Exp.Sizeof {typ=typ; nbytes=None; 
+              dynamic_length=None; subtype=Subtype.exact}))
+        end
+        
+      | _ -> failwith "Case not handled - not pointsto"
+      end
+
+    | _ -> failwith "Case not handled - not pointsto"
+  ) non_empty_post_sigma) in 
+
+  Prop.from_sigma hpred_list
+
+let rec synthesize_writes callbacks procspec (queue: Sil.instr list list) matches =
   let proc_desc = callbacks.Callbacks.proc_desc in 
   let proc_name = Procdesc.get_proc_name proc_desc in 
   let summary = Interproc.analyze_procedure callbacks in
@@ -136,49 +210,64 @@ let rec synthesize_writes callbacks node (queue: Sil.instr list list) =
   print_specs specs;
   let post = fst (List.hd_exn (List.hd_exn specs).Specs.posts) in
   let pre = Specs.Jprop.to_prop (List.hd_exn specs).Specs.pre in 
-  let my_new_post = create_new_post callbacks.Callbacks.tenv pre in 
 
+  (* This section is duplicated in the initial synthesize to count initial matches *)
+  let my_new_post = make_post procspec pre callbacks.Callbacks.tenv proc_desc in 
   match Prover.check_implication_for_footprint proc_name callbacks.Callbacks.tenv post my_new_post with 
   | ImplOK (checks, sub1, sub2, frame, missing_pi, missing_sigma,
     frame_fld, missing_fld, frame_typ, missing_typ) -> 
     F.print_string "post = given post: Yes\n";
+    F.print_string "Given post: \n";
+    Prop.pp_prop Pp.text F.std_formatter my_new_post;
+    F.print_string "\n"
   | ImplFail _ -> 
   F.print_string "post = given post: No\n";
 
-  let new_instrs = ref [] in
+  let new_matches = ref 0 in
   
   List.iter ~f:(fun hpred1 -> 
     List.iter ~f:(fun hpred2 ->
       if Sil.equal_hpred hpred1 hpred2 then 
         begin
-        new_instrs := Procdesc.Node.get_instrs node;
+        new_matches := !new_matches + 1;
         F.print_string "Match found! - ";
+        F.print_string "hpred1: ";
+        Sil.pp_hpred {Pp.text with opt = SIM_WITH_TYP} F.std_formatter hpred1;
+        F.print_string " && hpred2: ";
+        Sil.pp_hpred {Pp.text with opt = SIM_WITH_TYP} F.std_formatter hpred2;
+        F.print_string "\n"
         end;
-      F.print_string "hpred1 is: ";
-      Sil.pp_hpred {Pp.text with opt = SIM_WITH_TYP} F.std_formatter hpred1;
-      F.print_string " and hpred2 is: ";
-      Sil.pp_hpred {Pp.text with opt = SIM_WITH_TYP} F.std_formatter hpred2;
-      F.print_string "\n"
     ) my_new_post.sigma;
   ) post.sigma;
   F.print_string "\n";
 
   let abstract_inst = Sil.Abstract (Location.dummy) in 
-  let next_instrs = List.hd_exn queue in
-  new_instrs := !new_instrs @ next_instrs @ [abstract_inst];
-  Procdesc.Node.replace_instrs node !new_instrs;
+  let next_instrs = List.hd queue in
+  match next_instrs with 
+  | None -> failwith "Nothing left in synthesis queue"
+  | Some ni ->
+  let new_instrs = ni @ [abstract_inst] in 
+
+  begin
+  if !new_matches > matches then 
+    let write_node = Procdesc.create_node proc_desc Location.dummy (Procdesc.Node.Stmt_node "") 
+      new_instrs in 
+    insert_penultimate_node write_node proc_desc;
+    Procdesc.compute_distance_to_exit_node proc_desc;
+  else
+    let penultimate_node = 
+      List.hd_exn (Procdesc.Node.get_preds (Procdesc.get_exit_node proc_desc)) in 
+    Procdesc.Node.replace_instrs penultimate_node new_instrs;
+  end;
 
   F.print_string "Node instrs after\n";
   print_node_instrs proc_desc;
 
-  
   (* Recursive call to run - after the instructions have been changed *)
-  let new_queue = List.tl queue in 
-  match new_queue with
-  | None -> failwith "Nothing left in synthesis queue"
-  | Some nq -> synthesize_writes callbacks node nq
+  let new_queue = List.tl_exn queue in 
+  synthesize_writes callbacks procspec new_queue (max !new_matches matches)
 
-let synthesize proc_name exe_env = 
+let synthesize proc_name exe_env procspec = 
   let proc_name_readable = Typ.Procname.to_string proc_name in
   F.print_string ("Begin proc: " ^ proc_name_readable ^ "\n");
   let get_proc_desc proc_name = Exe_env.get_proc_desc exe_env proc_name in
@@ -206,11 +295,6 @@ let synthesize proc_name exe_env =
   | None -> failwith "No specs"
   | Some spec ->
   let pre = Specs.Jprop.to_prop spec.pre in
-  let posts = List.map ~f:fst spec.posts in
-  let post = List.hd posts in 
-  match post with
-  | None -> ()
-  | Some _ ->
 
   F.print_string "formals: ";
   List.iter ~f:(fun f -> Mangled.pp F.std_formatter (fst f); F.print_string " ") (Procdesc.get_formals proc_desc);
@@ -229,7 +313,8 @@ let synthesize proc_name exe_env =
     let typ = get_typ_from_ptr_exn p_typ in 
     [ Sil.Load (temp1, Exp.Lvar pvar, p_typ, Location.dummy);
     Sil.Load (temp2, Exp.Var temp1, typ, Location.dummy);
-    Sil.Store (Exp.Lvar local, typ, Exp.Var temp2, Location.dummy) ]
+    Sil.Store (Exp.Lvar local, typ, Exp.Var temp2, Location.dummy);
+    Sil.Remove_temps ([temp1; temp2], Location.dummy) ]
   ) pvars local_vars in 
   let abstract_instr = Sil.Abstract (Location.dummy) in 
 
@@ -242,9 +327,10 @@ let synthesize proc_name exe_env =
     List.map ~f:(fun local -> 
       let temp1 = Ident.create_fresh Ident.knormal in 
       let temp2 = Ident.create_fresh Ident.knormal in 
-      [Sil.Load (temp1, Exp.Lvar pv, get_typ_from_ptr_exn typ, Location.dummy);
+      [ Sil.Load (temp1, Exp.Lvar pv, get_typ_from_ptr_exn typ, Location.dummy);
       Sil.Load (temp2, Exp.Lvar local, get_typ_from_ptr_exn typ, Location.dummy);
-      Sil.Store (Exp.Var temp1, get_typ_from_ptr_exn typ, Exp.Var temp2, Location.dummy)]
+      Sil.Store (Exp.Var temp1, get_typ_from_ptr_exn typ, Exp.Var temp2, Location.dummy);
+      Sil.Remove_temps ([temp1; temp2], Location.dummy) ]
     ) local_vars) pvars) in
 
   let write_node = Procdesc.create_node proc_desc Location.dummy (Procdesc.Node.Stmt_node "") 
@@ -256,8 +342,37 @@ let synthesize proc_name exe_env =
   Specs.clear_spec_tbl ();
   let callbacks = { Callbacks.get_proc_desc; 
     get_procs_in_file; idenv; tenv; summary; proc_desc; } in
+
+  (* Re-analyze after putting in the initial reads *)
+  let specs = Specs.get_specs_from_payload (Interproc.analyze_procedure callbacks) in
+  let pre = Specs.Jprop.to_prop (List.hd_exn specs).Specs.pre in 
+  let post = fst (List.hd_exn (List.hd_exn specs).Specs.posts) in 
+
+  (* This section is duplicated from synthesize_writes in order to get the initial number of matches *)
+  let my_new_post = make_post procspec pre callbacks.Callbacks.tenv callbacks.Callbacks.proc_desc in 
+  match Prover.check_implication_for_footprint proc_name callbacks.Callbacks.tenv post my_new_post with 
+  | ImplOK _ -> 
+    F.print_string "post = given post: Yes\n";
+    F.print_string "Given post: \n";
+    Prop.pp_prop Pp.text F.std_formatter my_new_post;
+    F.print_string "\n"
+  | ImplFail _ -> 
+  F.print_string "post = given post: No\n";
+
+  let new_matches = ref 0 in
+  List.iter ~f:(fun hpred1 -> 
+    List.iter ~f:(fun hpred2 ->
+      if Sil.equal_hpred hpred1 hpred2 then 
+        begin
+        new_matches := !new_matches + 1;
+        end;
+    ) my_new_post.sigma;
+  ) post.sigma;
+  F.print_string "\n";
+
+  F.printf "INITIAL MATCHES: %d\n" !new_matches;
   
-  synthesize_writes callbacks write_node possible_writes
+  synthesize_writes callbacks procspec possible_writes !new_matches
 
 let run ~arg = 
   let pspec = ParseMain.run arg in 
@@ -288,7 +403,7 @@ let run ~arg =
     String.equal (Typ.Procname.to_string pn) proc_sig.id) procs_to_analyze with
   | None -> failwith ("No proc found with name " ^ proc_sig.id)
   | Some proc_name ->
-  synthesize proc_name exe_env
+  synthesize proc_name exe_env procspec
   
 
   
